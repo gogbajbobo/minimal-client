5fd77dcc5319bb8997eecf7ae9f1631bf4e9cba7
gogbajbobo <grigoriev.maxim@gmail.com>
2023-03-25 08:10:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // https://developer.mozilla.org/ru/docs/Web/Guide/AJAX/Getting_Started
 
+window.onload = () => makeRequest()
+
 const makeRequest = (url = 'http://localhost:3000') => {
 
     let httpRequest = false
@@ -33,13 +35,35 @@ const makeRequest = (url = 'http://localhost:3000') => {
 
 }
 
+const spanMessage = (message, isError) => {
+
+    const resultSpan = document.querySelector("#result")
+    if (isError)
+        resultSpan.style.color = 'red'
+    return resultSpan.innerHTML = message
+
+}
+
 const alertContents = httpRequest => {
 
     if (httpRequest.readyState === 4) {
         if (httpRequest.status === 200) {
-            alert(httpRequest.responseText)
+
+            try {
+
+                const { message, result, err } = JSON.parse(httpRequest.responseText)
+
+                if (err)
+                    return spanMessage(`${ message }<br>Response error: ${ err.message || err.sqlMessage }`, true)
+
+                return spanMessage(`${ message }<br>Server connected to '${ result }' database`)
+
+            } catch (err) {
+                return spanMessage(`Response parse error: ${ err.message }`, true)
+            }
+
         } else {
-            alert(`There was a problem with the request, status: ${ httpRequest.status }`)
+            return spanMessage(`There was a problem with the request, status: ${ httpRequest.status }`, true)
         }
     }
 
